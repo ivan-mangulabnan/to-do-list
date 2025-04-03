91eb1f77e05faa2a41be397c5b9013434267b1d8
ivan <ivanmangulabnan19@gmail.com>
2025-04-03 17:48:47 +0800

    Add a function the creates a whole project

--- src/scripts/dom-nodes.js
+++ src/scripts/dom-nodes.js
@@ -1,7 +1,8 @@
 import { Todos } from "./todos.js";
 import { Projects } from "./projects.js";
 import { Priority } from "./priority.js";
-import { Validation } from "./validation.js"; 
+import { Validation } from "./validation.js";
+import * as dateFns from "date-fns";
 
 export class Dom {
     
@@ -48,6 +49,9 @@ export class Dom {
     }
 
     static initialize() {
+        const homeButton = document.querySelector("#home-link");
+        homeButton.click(); //sets the initialization with design.
+
         Nav.showProjects();
 
         if (Todos.isEmpty() === true) {
@@ -56,6 +60,215 @@ export class Dom {
             Dom.displayAllProjectWithTodos();
             Dom.displayTodo();
         }
+    }
+
+    static loadFullProject(event) {
+        if (!event.target.matches("a")) return
+        Dom.contentDiv.innerHTML = "";
+        Dom.contentDiv.classList.remove("no-task");
+
+        const project = event.target.textContent;
+        const filteredTodos = Todos.list.filter(todo => todo.project === project);
+        
+        const mainDiv = document.createElement("div");
+        mainDiv.classList.add("projectMainDiv");
+        const projectTitleDiv = document.createElement("div");
+        projectTitleDiv.classList.add("projectTitleDiv");
+        const todoDiv = document.createElement("div");
+
+        const h3Div = document.createElement("div");
+        const h3 = document.createElement("h3");
+        h3.classList.add("project-name");
+        h3.textContent = project;
+
+        const btnsDiv = document.createElement("div");
+        btnsDiv.classList.add("btnsDiv");
+        const editBtn = document.createElement("button");
+        editBtn.classList.add("project-edit-btn");
+        const delBtn = document.createElement("button");
+        delBtn.classList.add("delBtn");
+
+        if (filteredTodos.length === 0) {
+            todoDiv.classList.add("empty");
+            const p = document.createElement("p");
+            p.textContent = `${project} Project is currently empty`;
+            p.classList.add("zero-task");
+            todoDiv.appendChild(p);
+        } else {
+            todoDiv.classList.add("withTodo");
+
+            filteredTodos.forEach(todo => {
+
+                const todoWrapper = document.createElement("div");
+                todoWrapper.classList.add("todo-wrapper");
+                todoWrapper.classList.add("todo");
+
+                const titleDiv = document.createElement("div");
+                titleDiv.classList.add("todo-main-div"); // category div class
+
+                const titleHeader = document.createElement("p");
+                titleHeader.classList.add("todo-headers"); // all header class
+                titleHeader.textContent = "TITLE";
+
+                const titlePWrapper = document.createElement("div");
+                titlePWrapper.classList.add("todo-content-wrapper"); // content wrapper class
+
+                const titleP = document.createElement("p");
+                titleP.classList.add("todo-content-item"); // content class
+                titleP.textContent = todo.title;
+
+                titlePWrapper.appendChild(titleP);
+                titleDiv.append(titleHeader, titlePWrapper);
+
+                const datePrioDiv = document.createElement("div"); // Date and Priority Here Starts Here.
+                datePrioDiv.classList.add("date-prio-div");
+
+                const dateDiv = document.createElement("div"); // Date starts Here
+                dateDiv.classList.add("todo-main-div");
+
+                const dateHeader = document.createElement("p");
+                dateHeader.textContent = "DUE DATE";
+                dateHeader.classList.add("todo-headers");
+
+                const datePWrapper = document.createElement("div");
+                datePWrapper.classList.add("todo-content-wrapper");
+
+                const dateP = document.createElement("p");
+                dateP.textContent = dateFns.format(todo.dueDate, "MMMM d, yyyy");
+                dateP.classList.add("todo-content-item");
+                
+                datePWrapper.appendChild(dateP);
+                dateDiv.append(dateHeader, datePWrapper);
+
+                const prioDiv = document.createElement("div"); // Priority starts Here
+                prioDiv.classList.add("todo-main-div");
+
+                const prioHeader = document.createElement("p");
+                prioHeader.textContent = "PRIORITY";
+                prioHeader.classList.add("todo-headers");
+
+                const prioPWrapper = document.createElement("div");
+                prioPWrapper.classList.add("todo-content-wrapper");
+
+                const prioP = document.createElement("p");
+                prioP.textContent = todo.priority;
+                prioP.classList.add("todo-content-item");
+
+                switch(todo.priority) {
+                    case "Top Priority":
+                        prioPWrapper.classList.add("top");
+                    break
+                    case "Medium Priority":
+                        prioPWrapper.classList.add("mid");
+                    break
+                    case "Low Priority":
+                        prioPWrapper.classList.add("low");
+                    break
+                }
+                
+                prioPWrapper.appendChild(prioP);
+                prioDiv.append(prioHeader, prioPWrapper);
+
+                datePrioDiv.append(dateDiv, prioDiv);
+
+                todoWrapper.append(titleDiv, datePrioDiv);
+                todoDiv.appendChild(todoWrapper);
+
+                if (todo.description !== "") {
+                    const descriptionDiv = document.createElement("div"); // Description starts Here
+                    descriptionDiv.classList.add("todo-main-div");
+    
+                    const descriptionHeader = document.createElement("p");
+                    descriptionHeader.textContent = "DESCRIPTION";
+                    descriptionHeader.classList.add("todo-headers");
+    
+                    const descriptionPWrapper = document.createElement("div");
+                    descriptionPWrapper.classList.add("todo-content-wrapper");
+    
+                    const descriptionP = document.createElement("p");
+                    descriptionP.textContent = todo.description;
+                    descriptionP.classList.add("todo-content-item");
+                    
+                    descriptionPWrapper.appendChild(descriptionP);
+                    descriptionDiv.append(descriptionHeader, descriptionPWrapper);
+
+                    todoWrapper.appendChild(descriptionDiv);
+                }
+
+                if (todo.notes !== "") {
+                    const notesDiv = document.createElement("div"); // Note starts Here
+                    notesDiv.classList.add("todo-main-div");
+    
+                    const notesHeader = document.createElement("p");
+                    notesHeader.textContent = "NOTES";
+                    notesHeader.classList.add("todo-headers");
+    
+                    const notesPWrapper = document.createElement("div");
+                    notesPWrapper.classList.add("todo-content-wrapper");
+    
+                    const notesP = document.createElement("p");
+                    notesP.textContent = todo.notes;
+                    notesP.classList.add("todo-content-item");
+                    
+                    notesPWrapper.appendChild(notesP);
+                    notesDiv.append(notesHeader, notesPWrapper);
+                    
+                    todoWrapper.append(notesDiv);
+                }
+
+                if (todo.checklist !== null && todo.checklist.length !== 0) {
+                    const checkDiv = document.createElement("div"); // Checklist starts Here
+                    checkDiv.classList.add("todo-main-div");
+    
+                    const checkHeader = document.createElement("p");
+                    checkHeader.textContent = "CHECKLISTS";
+                    checkHeader.classList.add("todo-headers");
+    
+                    const checkPWrapper = document.createElement("div");
+                    checkPWrapper.classList.add("todo-content-wrapper");
+                    checkPWrapper.classList.add("check-wrapper");
+    
+                    todo.checklist.forEach((checklist, index) => {
+                        const chkDiv = document.createElement("div");
+                        chkDiv.classList.add("chk-div");
+                        const status = document.createElement("p");
+                        status.classList.add("chk-status");
+    
+                        if (checklist.status) {
+                            status.textContent = "COMPLETED";
+                            status.classList.add("completed");
+                        } else {
+                            status.textContent = "IN PROGRESS";
+                            status.classList.add("in-progress");
+                        }
+    
+                        const checkp = document.createElement("p");
+                        checkp.classList.add("chkp");
+                        let text = `${index + 1}. `;
+    
+                        for (let key in checklist) {
+                            if (key === "text") text += checklist[key];
+                            if (key === "quantity" && checklist[key] !== "") text += ` ${checklist[key]}x `;
+                        }
+    
+                        checkp.textContent = text;
+                        chkDiv.append(status, checkp);
+                        checkPWrapper.appendChild(chkDiv);
+                    })
+    
+                    checkDiv.append(checkHeader, checkPWrapper);
+                    todoWrapper.append(checkDiv);
+                }
+
+                
+            })
+        }
+
+        btnsDiv.append(editBtn, delBtn);
+        h3Div.appendChild(h3);
+        projectTitleDiv.append(h3Div, btnsDiv);
+        mainDiv.append(projectTitleDiv, todoDiv);
+        Dom.contentDiv.appendChild(mainDiv);
     }
 
     static loadFormOptions() {
