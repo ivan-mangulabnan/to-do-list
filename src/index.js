6c62baf6021f5a63e1e6fbe61f1cec65805a51a0
ivan <ivanmangulabnan19@gmail.com>
2025-04-05 23:13:47 +0800

    Add event listeners for editDom

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import "./index.css";
 import { Dom, FormDom, Nav } from "./scripts/dom-nodes.js";
 import { saveTodo } from "./scripts/create-todo.js";
+import { Validation } from "./scripts/validation.js";
+import { Projects } from "./scripts/projects.js";
 
 document.addEventListener("DOMContentLoaded", event => { // initialze the page.
     Dom.initialize();;
@@ -63,4 +65,82 @@ homeButton.addEventListener("click", Dom.initialize);
 const navProjectDiv = document.querySelector("#nav-projects"); // Project Navs
 navProjectDiv.addEventListener("click", event => {
     Dom.loadFullProject(event);
+})
+
+Dom.contentDiv.addEventListener("click", event => { // Show Edit Dom
+    if (event.target.matches(".modify-button")) Dom.editTodoDisplay(event.target);
+})
+
+document.addEventListener("change", event => { // Changing the status on editDom
+    if (event.target.matches(".edit-completion-status")) {
+        const p = event.target.nextElementSibling;
+        p.textContent = event.target.checked === true ? "COMPLETED" : "NOT YET COMPLETED";
+    }
+})
+
+document.addEventListener("click", event => { // Show Add Project in Edit
+    if (event.target.matches(".edit-add-proj") || event.target.matches(".edit-minus-proj")) {
+        const newProj = document.querySelector(".edit-new-proj");
+        if (event.target.textContent === "✚") {
+            event.target.textContent = "-";
+            event.target.className = "";
+            event.target.classList.add("edit-minus-proj");
+            newProj.classList.toggle("show-display");
+            return
+        }
+
+        event.target.textContent = "✚";
+        event.target.className = "";
+        event.target.classList.add("edit-add-proj");
+        newProj.classList.toggle("show-display");
+    }
+})
+
+document.addEventListener("keyup", event => { // Escape key
+    if (event.key === "Escape") {
+        const editDialog = document.querySelector(".edit-dialog");
+        if (editDialog === null) return
+        editDialog.remove();
+    }
+})
+
+document.addEventListener("click", event => {
+    if (event.target.matches(".plus-proj")) { // Add New Proj in Edit
+        const input = document.querySelector(".plus-proj-input");
+        const btn = document.querySelector(".edit-minus-proj");
+        const select = document.querySelector(".edit-item-select");
+
+        if (!Validation.notEmpty(input.value)) {
+            input.setCustomValidity("This field cannot be empty.");
+            input.reportValidity();
+            return
+        }
+
+        Projects.add(input.value);
+        select.innerHTML = "";
+        Projects.list.forEach(project => {
+            const opt = document.createElement("option");
+            opt.value = project;
+            opt.textContent = project;
+            select.appendChild(opt);
+        })
+        input.value = "";
+        Dom.initialize();
+        btn.click();
+    }
+
+    if (event.target.matches(".checklist-plus")) { // creates new checklist in edit
+        const chkDiv = document.querySelector(".checklist-chkdiv");
+        FormDom.utilChecklist(chkDiv, false, "", "");
+    }
+
+    if (event.target.matches(".rmv-btn")) { // checklist item remove.
+        const targetChecklist = event.target.closest(".checklist-single-div");
+        targetChecklist.remove();
+    }
+
+    if (event.target.matches(".edit-rmv-btn")) { // whole dialog remove.
+        const editDialog = document.querySelector(".edit-dialog");
+        editDialog.remove();
+    }
 })
